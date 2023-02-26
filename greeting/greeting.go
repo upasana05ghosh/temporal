6756f9e1c94002d5ee9a1c108f80bf954163f276
Upasana Ghosh <upasana05ghosh@gmail.com>
2023-02-26 20:04:06 +0530

    05 Create Activity and workflow

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -1,9 +1,59 @@
 package greeting
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+
 	"go.temporal.io/sdk/workflow"
 )
 
 func Greet(ctx workflow.Context, name string) (string, error) {
 	return "hello " + name, nil
+}
+
+// Activity
+// context is first arg, it's optional but recommended to pass imp info from temporal workflow
+func GreetInSpanish(ctx context.Context, name string) (string, error) {
+	base := "http://localhost:9999/get-spanish-greeting?name=%s"
+	url := fmt.Sprintf(base, url.QueryEscape(name))
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	translation := string(body)
+	status := resp.StatusCode
+	if status >= 400 {
+		message := fmt.Sprintf("HTTP Error %d: %s", status, translation)
+		return "", errors.New(message)
+	}
+	return translation, nil
+}
+
+// workflow which calls activity
+func GreetSomeone(ctx workflow.Context, name string) (string, error) {
+	options := workflow.ActivityOptions{
+		StartToCloseTimeout: time.Second * 5,
+	}
+	ctx = workflow.WithActivityOptions(ctx, options)
+	var spanishGreeting string
+
+	//ctx -> workflow.WithActivityOptions, nameOfFunc, Input param
+	//get -> on future object
+	err := workflow.ExecuteActivity(ctx, GreetInSpanish, name).Get(ctx, &spanishGreeting)
+	if err != nil {
+		return "", err
+	}
+	return spanishGreeting, nil
 }
